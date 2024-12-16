ef05af5db425111b81a491e5a8ac630db3600987
Evgeny Rezovsky <e.rezovsky@gmail.com>
2024-12-16 23:05:01 +0500

    create render module

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 import subprocess
 import os
 from modules.files import Files
+from modules.render import Render
 
 app = FastAPI()
 
@@ -43,19 +44,5 @@ async def find_files():
 
 @app.get("/render/")
 async def render_scene():
-    # Путь к сцене, которая будет рендериться
-    scene_path = "/blend_files/panelka.blend"
-    output_path = "blend_files/output/render_####.png"
-    
-    # Команда для рендеринга с использованием Blender
-    command = [
-        "blender", "-b", scene_path, "-o", output_path, "-F", "PNG", "-f", "1"
-    ]
-    
-    # Запускаем процесс рендеринга
-    result = subprocess.run(command, capture_output=True, text=True)
-    
-    if result.returncode == 0:
-        return {"message": "Render started successfully", "output": result.stdout}
-    else:
-        return {"message": "Render failed", "error": result.stderr}
+    render = Render("blend_files/scene.blend", "scene")
+    return await render.render_one_frame()
---
+++ modules/render.py
@@ -0,0 +1,16 @@
+import subprocess
+
+class Render():
+    def __init__(self, file_name: str, output_file: str):
+        self.file_name = file_name
+        self.output_path = output_path
+        
+    
+    async def render_one_frame(self):
+        command = ["blender", "-b", self.file_name, "-o", f"output/{self.output_file}.png", "-F", "PNG", "-f", "1"]
+        result = subprocess.run(command, capture_output=True, text=True)
+        
+        if result.returncode == 0:
+            return {"message": "Render started successfully", "output": result.stdout}
+        else:
+            return {"message": "Render failed", "error": result.stderr}
