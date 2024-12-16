29df32d3f255ab3fa517c87d9fdd15c73109077a
Evgeny Rezovsky <e.rezovsky@gmail.com>
2024-12-16 22:34:03 +0500

    install vue

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import subprocess
 import os
 
 app = FastAPI()
 
-@app.get("/find_files/")
+origins = [
+    "http://localhost:8765"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+css_directory = os.path.join(os.getcwd(), "web", "dist", "css")
+app.mount("/css", StaticFiles(directory=css_directory), name="css")
+js_directory = os.path.join(os.getcwd(), "web", "dist", "js")
+app.mount("/js", StaticFiles(directory=js_directory), name="js")
+static_directory = os.path.join(os.getcwd(), "web", "dist")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    html_file_path = os.path.join(static_directory, "index.html")
+    if os.path.exists(html_file_path):
+        with open(html_file_path, "r", encoding="utf-8") as file:
+            content = file.read()
+        return HTMLResponse(content=content)
+    return HTMLResponse(content="Index file not found.", status_code=404)
+
+@app.get("/find_files")
 async def find_files():
     folder_path = "blend_files"
     
