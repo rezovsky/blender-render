f3360ef0795e998ec92ec7e2cc8eb4d3e191a11c
rezovsky <e.rezovsky@gmail.com>
2025-03-18 15:10:50 +0500

    edit corde

--- main.py
+++ main.py
@@ -23,9 +23,7 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000"  # если фронт в dev режиме на Vite
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
