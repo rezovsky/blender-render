1056aa36c45dd206b00ae2b948f03df4d306d0b9
rezovsky <e.rezovsky@gmail.com>
2025-03-18 14:25:57 +0500

    migrate vue cli to vite

---
+++ web/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true, // доступ из локальной сети
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
